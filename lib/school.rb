e85f3aa184790824e604ff9ccac741fe79ee9388
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-17 07:53:01 +1000

    Lab completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :school_name, :roster
+  def initialize(school_name, roster = {})
+    @school_name = school_name
+    @roster = roster
+  end
+
+  def add_student(student_name, grade)
+    !@roster[grade] ? @roster[grade] = [student_name] : @roster[grade].push(student_name)
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, studentArr|
+      @roster[grade] = studentArr.sort
+    end
+  end
+end
